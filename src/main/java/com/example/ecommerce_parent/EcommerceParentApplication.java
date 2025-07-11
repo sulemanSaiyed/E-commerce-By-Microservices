494593bd4fb0ff4f37160b13f426998d13853e0d
Suleman Saiyed <sulemansaiyed20@gmail.com>
2025-07-11 15:58:30 +0530

    initial commit

---
+++ src/main/java/com/example/ecommerce_parent/EcommerceParentApplication.java
@@ -0,0 +1,13 @@
+package com.example.ecommerce_parent;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EcommerceParentApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EcommerceParentApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/ecommerce_parent/EcommerceParentApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.ecommerce_parent;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EcommerceParentApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
