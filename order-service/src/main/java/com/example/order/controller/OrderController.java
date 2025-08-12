2bb4fb6f325e8736e60f6da093ec24738439710e
pandu-12-2000 <crackivar@gmail.com>
2025-08-12 23:02:31 +0530

    removed TODO comment for create order - feature already implemented

--- order-service/src/main/java/com/example/order/controller/OrderController.java
+++ order-service/src/main/java/com/example/order/controller/OrderController.java
@@ -15,10 +15,7 @@ import org.springframework.web.bind.annotation.RestController;
 public class OrderController {
     private final OrderService orderService;
 
-    // TODO: Implement the logic to handle the order creation
-    /*
-     * In order to create order, the endpoint should receive the CartItem ID, The CartItem should
-     * be found based on the ID and the order should be created with the cart items. */
+
     @PostMapping("/orders")
     public ResponseEntity<OrderResponse> createOrder() {
         OrderResponse response = orderService.createOrder();
